24da9f86efe2d0d0132a1f2ee6d28e901289650b
Tucker Vassau <tuckv@hotmail.com>
2020-01-05 18:08:20 -0700

    Current Weather API call working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 // JAVASCRIPT
-$(".btn").on("click", function(){
+var queryUrlBase = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var apikey = '&appid=0cf78313188ed7c923c873cd418f1e41';
+
+function currentWeather(cWU){
+    $.ajax({
+        url: cWU,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+    })
+};
+
+$(".btn").on("click", function(event){
     alert("Button Pressed");
+
+    event.preventDefault();
+    var city = $('#citySearched').val().trim();
+    console.log(city);
+
+    var currentWeatherUrl = queryUrlBase + city + "&units=imperial" + apikey;
+    console.log(currentWeatherUrl);
+    currentWeather(currentWeatherUrl);
+
 })
