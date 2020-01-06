65d2ef0f024033d74a6a1cb833e382cd7c6df809
Tucker Vassau <tuckv@hotmail.com>
2020-01-05 22:44:47 -0700

    Both API Calls Functioning

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 // JAVASCRIPT
 var queryUrlBase = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var fiveDayUrlBase = 'http://api.openweathermap.org/data/2.5/forecast?q='
 var apikey = '&appid=0cf78313188ed7c923c873cd418f1e41';
 
 function currentWeather(cWU){
@@ -7,7 +8,18 @@ function currentWeather(cWU){
         url: cWU,
         method: "GET"
     }).then(function(response){
+        console.log("Current Weather: ")
         console.log(response);
+    })
+};
+
+function fiveDayWeather(fDWU){
+    $.ajax({
+        url: fDWU,
+        method: "GET"
+    }).then(function(responseFive){
+        console.log("Five Day Forecast: ")
+        console.log(responseFive);
     })
 };
 
@@ -21,5 +33,9 @@ $(".btn").on("click", function(event){
     var currentWeatherUrl = queryUrlBase + city + "&units=imperial" + apikey;
     console.log(currentWeatherUrl);
     currentWeather(currentWeatherUrl);
+
+    var fiveDayWeatherUrl = fiveDayUrlBase + city + "&units=imperial" + apikey;
+    console.log(fiveDayWeatherUrl);
+    fiveDayWeather(fiveDayWeatherUrl);
 
 })
